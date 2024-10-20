30cb958becd610e8a61b28ca4149108d89cfb2fc
Noah Steffes <noah@getox.com>
2024-10-19 22:01:35 -0500

    try to fix media

--- tina/config.js
+++ tina/config.js
@@ -24,6 +24,14 @@ export default defineConfig({
       publicFolder: "exampleSite/static",
     },
   },
+  search: {
+    tina: {
+      indexerToken: process.env.TINA_SEARCH_TOKEN,
+      stopwordLanguages: ["eng"],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100,
+  },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
